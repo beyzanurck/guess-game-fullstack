be8ddb172d5aecd47debb019f0aaea38877279e9
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-11-14 18:51:12 -0600

    creates button and state to display top players

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 import Board from './Board'
 import PlayerList from './PlayerList'
 import { useState, useEffect } from 'react';
+import TopPlayers from './TopPlayers';
 
 
 function App() {
   const [lastScore, setLastScore] = useState(0);
+  const [show, setShow] = useState(false)
+
 
   const randomInt =  Math.floor(Math.random() * 101)
 
@@ -21,6 +24,13 @@ function App() {
     <div>
       <PlayerList lastScore = {lastScore}/>
       <Board random = {randomInt} getScore = {passScore}/>
+
+      <button onClick={()=> setShow(true)}>Show Tops</button>
+
+      {
+        show &&
+        <TopPlayers />
+      }
 
     </div>
   )
