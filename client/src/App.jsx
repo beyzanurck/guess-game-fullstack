73c26bb8bd423628c54db6c3f2b31e3685eca5c2
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-11-14 19:19:08 -0600

    displays new player component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import Board from './Board'
 import PlayerList from './PlayerList'
 import { useState, useEffect } from 'react';
 import TopPlayers from './TopPlayers';
+import NewPlayer from './NewPlayer';
 
 
 function App() {
   const [lastScore, setLastScore] = useState(0);
   const [show, setShow] = useState(false)
+  const [showNewPlayer, setShowNewPlayer] = useState(false)
+
 
 
   const randomInt =  Math.floor(Math.random() * 101)
@@ -23,6 +26,14 @@ function App() {
   return (
     <div>
       <PlayerList lastScore = {lastScore}/>
+
+      <button onClick={()=> setShowNewPlayer(true)}>New Player</button>
+      {
+        showNewPlayer&&
+        <NewPlayer onClose={() => setShowNewPlayer(false)}/>
+      }
+
+
       <Board random = {randomInt} getScore = {passScore}/>
 
       <button onClick={()=> setShow(true)}>Show Tops</button>
