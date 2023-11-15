0231b73c5fe3472d72f7250d2397cafeb4dd3e5e
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-11-14 19:03:25 -0600

    fetches top players

--- client/src/TopPlayers.jsx
+++ client/src/TopPlayers.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function TopPlayers() {
+
+    const [topPlayers, setTopPLayers] = useState([])
+
+    const getTopPlayers = async () => {
+        try {
+            const response = await fetch('http://localhost:1212/top-players');
+            const data = await response.json();
+            setTopPLayers(data);
+        } catch (error) {
+            console.error('Failed to fetch players', error);
+        }
+    };
+
+    useEffect(() => {
+        getTopPlayers()
+        console.log(topPlayers)
+    }, [])
   return (
     <div>
+
+        {
+            topPlayers.length > 0 ? 
+            topPlayers.map((item, index) => (
+            <p key={index}> {item.nickname} - {item.score} </p>
+          )) : ` `
+        }
      
     </div>
   )
